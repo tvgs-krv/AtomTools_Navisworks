ee2b918d00f5cfdd0a412f5140dfb15bc5dccc2b
Roman.K <47116442+tvgs-krv@users.noreply.github.com>
2020-04-23 14:21:04 +0300

    #start
    first commit

---
+++ AtomTools_Navisworks/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtomTools_Navisworks
+{
+    public class Class1
+    {
+    }
+}
