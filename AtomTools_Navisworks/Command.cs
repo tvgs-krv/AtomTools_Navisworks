e3a54849fbf5036bbb6dfb0df4b25aee4ad07177
Roman.K <47116442+tvgs-krv@users.noreply.github.com>
2020-04-23 15:59:32 +0300

    #Add main ribbon on panel

---
+++ AtomTools_Navisworks/Command.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Autodesk.Navisworks.Api.Plugins;
+
+namespace AtomTools_Navisworks
+{
+    [Plugin("AtomTools_Navisworks", "ASE", DisplayName = "AtomTools")]
+    [RibbonLayout("AtomTools_Navisworks.xaml")]
+    [RibbonTab("AtomTools", DisplayName = "AtomTools")]
+
+    [Command("ID_Button_1", CanToggle = true, DisplayName = "MENU", Icon = "CopyParameters_64.png", ToolTip = "Main menu for plugins")]
+
+    public class Command : CommandHandlerPlugin
+    {
+
+        public override int ExecuteCommand(string name, params string[] parameters)
+        {
+            MessageBox.Show("s");
+            return 0;
+        }
+    }
+}
